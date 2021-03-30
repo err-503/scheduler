1f90b35e0f4fd85f6ca90fce3907c435010d29fd
err-503 <66683132+err-503@users.noreply.github.com>
2021-03-30 20:42:25 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const schedule = require('node-schedule');
 
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-rule.hour = 15;
+rule.hour = 12;
 rule.minute = 0;
 
 const prefix = "!hu";
@@ -23,7 +23,16 @@ client.on("message", msg => {
 		scheduledText();
 	}
 	if(msg.content === `${prefix} help`){
-		msg.reply("Tai aš, 'hu?' bot'as! Kiekvieną dieną 15:00, aš paklausiu, ar nori žaisti. Pažymėk savo atsakymą, ir po trijų valandų aš atsiųsiu statistiką!")
+		msg.reply("Tai aš, 'hu?' bot'as! Kiekvieną dieną 15:00, aš paklausiu, ar nori žaisti. Pažymėk savo atsakymą, ir po dviejų valandų aš atsiųsiu statistiką!")
+	}
+	if(msg.content === `amogus`){
+		msg.reply("https://static.wikia.nocookie.net/jerma-lore/images/e/e3/JermaSus.jpg/revision/latest?cb=20201206225609")
+	}
+	if(msg.content === `a m o g u s`){
+		msg.reply("https://external-preview.redd.it/-fyP2iR_I19APf_9k7EFdND6wa_ir1bqDGG09729bT4.png?width=320&crop=smart&format=pjpg&auto=webp&s=96a7866a47ac43163aec1121a62bf63046427163")
+	}
+	if(msg.content === `amogus drip`){
+		msg.reply("https://cdn.discordapp.com/attachments/810096035128803358/824536054596894740/video0-8.mp4")
 	}
 });
 
@@ -35,7 +44,7 @@ function scheduledText(){
 					sentMessage.react('✅');
 					sentMessage.react('❌');
 					const filter = (reaction, user) => reaction.emoji.name === '✅';
-					const collector = sentMessage.createReactionCollector(filter, { time: 10800000, dispose: true });
+					const collector = sentMessage.createReactionCollector(filter, { time: 7200000, dispose: true });
 					var people = -1;
 					console.log(`zmones: ${people}`);
 					collector.on('collect', () => {
