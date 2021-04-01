8fa6557716d329415539156eed7c31f6c84f2a88
err-503 <66683132+err-503@users.noreply.github.com>
2021-04-01 11:05:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const schedule = require('node-schedule');
 
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-rule.hour = 12;
+rule.hour = 13;
 rule.minute = 0;
 
+var peopleYes;
+var peopleMaybe;
+let memeImage;
+
 const prefix = "!hu";
+const memes = new Discord.MessageEmbed()
 
 client.on("ready", () => {
 	console.log("Bot online and active!");
@@ -23,49 +28,67 @@ client.on("message", msg => {
 		scheduledText();
 	}
 	if(msg.content === `${prefix} help`){
-		msg.reply("Tai aš, 'hu?' bot'as! Kiekvieną dieną 15:00, aš paklausiu, ar nori žaisti. Pažymėk savo atsakymą, ir po dviejų valandų aš atsiųsiu statistiką!")
+		msg.reply("Tai aš, 'hu?' bot'as! Kiekvieną dieną 16:00, aš paklausiu, ar nori žaisti. Pažymėk savo atsakymą, ir po trijų valandų aš atsiųsiu statistiką!")
 	}
-	if(msg.content === `amogus`){
-		msg.reply("https://static.wikia.nocookie.net/jerma-lore/images/e/e3/JermaSus.jpg/revision/latest?cb=20201206225609")
+	if(msg.content === `amogus` || msg.content === `sus`){
+		memeImage = "https://static.wikia.nocookie.net/jerma-lore/images/e/e3/JermaSus.jpg/revision/latest?cb=20201206225609";
+		memes.setImage(memeImage);
+		msg.channel.send(memes);
 	}
 	if(msg.content === `a m o g u s`){
-		msg.reply("https://external-preview.redd.it/-fyP2iR_I19APf_9k7EFdND6wa_ir1bqDGG09729bT4.png?width=320&crop=smart&format=pjpg&auto=webp&s=96a7866a47ac43163aec1121a62bf63046427163")
+		memeImage = "https://external-preview.redd.it/-fyP2iR_I19APf_9k7EFdND6wa_ir1bqDGG09729bT4.png?width=320&crop=smart&format=pjpg&auto=webp&s=96a7866a47ac43163aec1121a62bf63046427163";
+		memes.setImage(memeImage)
+		msg.channel.send(memes);
 	}
 	if(msg.content === `amogus drip`){
-		msg.reply("https://cdn.discordapp.com/attachments/810096035128803358/824536054596894740/video0-8.mp4")
+		memes.video("https://cdn.discordapp.com/attachments/810096035128803358/824536054596894740/video0-8.mp4");
+		msg.channel.send(memes);
 	}
 });
 
 function scheduledText(){
 	client.channels.fetch(process.env.CHANNEL)
 		.then(channel => {
-			channel.send("@everyone Kas žais šiandien?")
+			channel.send("@Nuolatinis Kas žais šiandien?")
 				.then(sentMessage => {
 					sentMessage.react('✅');
 					sentMessage.react('❌');
+					sentMessage.react('❔');
 					const filter = (reaction, user) => reaction.emoji.name === '✅';
-					const collector = sentMessage.createReactionCollector(filter, { time: 7200000, dispose: true });
-					var people = -1;
-					console.log(`zmones: ${people}`);
-					collector.on('collect', () => {
-						people = people + 1;
-						console.log(`zmones: ${people}`);
+					const collectorYes = sentMessage.createReactionCollector(filter, { time: 10800001, dispose: true });
+					peopleYes = -1;
+					console.log(`zmones: ${peopleYes}`);
+					collectorYes.on('collect', () => {
+						peopleYes = peopleYes + 1;
+						console.log(`zmones: ${peopleYes}`);
 					});
-					collector.on('remove', () => {
-						people = people - 1;
-						console.log(`zmones: ${people}`);
+					collectorYes.on('remove', () => {
+						peopleYes = peopleYes - 1;
+						console.log(`zmones: ${peopleYes}`);
 					});
-					collector.on('end', () => {
-						channel.send(`Šiandien žais ${people}`)
+					collectorYes.on('end', () => {
+						channel.send(`Šiandien tikrai žais ${peopleYes} ir gal žais ${peopleMaybe}.`)
 							.then(() =>{
-								if(people < 5){
+								if(peopleYes + peopleMaybe < 5){
 									channel.send("5-stack nesusidaro 😦");
-								} else if(people === 5){
+								} else if(peopleYes + peopleMaybe === 5){
 									channel.send("5-stack yra 😄");
 								} else {
 									channel.send("Teks daryt dvi komandas!");
 								}
 							})
+					});
+					const filter = (reaction, user) => reaction.emoji.name === '❔';
+					const collectorMaybe = sentMessage.createReactionCollector(filter, { time: 10800000, dispose: true });
+					peopleMaybe = -1;
+					console.log(`zmones: ${peopleMaybe}`);
+					collectorMaybe.on('collect', () => {
+						peopleMaybe = peopleMaybe + 1;
+						console.log(`zmones: ${peopleMaybe}`);
+					});
+					collectorMaybe.on('remove', () => {
+						peopleMaybe = peopleMaybe - 1;
+						console.log(`zmones: ${peopleMaybe}`);
 					});
 				});
 		});
